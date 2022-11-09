20afcf9c14bb14ad6785f9b4798531b2add883d4
Yunuel-Soto <brianyunuelsotosanchezz@gmail.com>
2022-11-09 08:54:27 -0600

    Tienda de productos/articulos terminada

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -5,53 +5,160 @@
 @section('contenido')
 
 <div class="cont-tienda">
+    <h1 class="h1-tienda"></h1>
     <div class="row row-cols-1 row-cols-md-3 g-4 cont-cards">
-
         <div class="col">
-          <div class="card" id="card-color">
+            <div class="card" id="card-color">
                 <img src="/img/spiderman.jpg" class="card-img-top img-card" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">Comic #1 spiderman</h5>
+                    <h5 class="card-title">Comic #1 Amazing Fantasy</h5>
                     <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
                     <div class="tienda-selec">
                         <button type="button" class="btn btn-primary">Comprar</button>
-                        <div>
-                            <a href="">Eliminar</a>
-                            <a href="">Actualizar</a>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
                         </div>
                     </div>
                 </div>
-          </div>
+            </div>
         </div>
 
         <div class="col">
-          <div class="card" id="card-color">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="card" id="card-color">
+                <img src="/img/superman.jpeg" class="card-img-top img-card" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Comic #116 Superman Infinitte Crisis</h5>
+                    <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
+                    <div class="tienda-selec">
+                        <button type="button" class="btn btn-primary">Comprar</button>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+        </div>
+
+
+        <div class="col">
+            <div class="card" id="card-color">
+                <img src="/img/spiderman.jpeg" class="card-img-top img-card" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Comic #20 The Amazing Spiderman</h5>
+                    <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
+                    <div class="tienda-selec">
+                        <button type="button" class="btn btn-primary">Comprar</button>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="col">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+            <div class="card" id="card-color">
+                <img src="/img/Avengers.jpeg" class="card-img-top img-card" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Comic #5 The Avengers</h5>
+                    <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
+                    <div class="tienda-selec">
+                        <button type="button" class="btn btn-primary">Comprar</button>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
 
         <div class="col">
-          <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <div class="card" id="card-color">
+                <img src="/img/Secret.jpeg" class="card-img-top img-card" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Comic #20 Secret Avengers</h5>
+                    <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
+                    <div class="tienda-selec">
+                        <button type="button" class="btn btn-primary">Comprar</button>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+        </div>
+
+        <div class="col">
+            <div class="card" id="card-color">
+                <img src="/img/x-men.jpeg" class="card-img-top img-card" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Comic #24 X-men</h5>
+                    <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
+                    <div class="tienda-selec">
+                        <button type="button" class="btn btn-primary">Comprar</button>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" id="card-color">
+                <img src="/img/Pinocho.jpeg" class="card-img-top img-card" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Funco de Pinocho</h5>
+                    <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
+                    <div class="tienda-selec">
+                        <button type="button" class="btn btn-primary">Comprar</button>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" id="card-color">
+                <img src="/img/DocStrange.jpeg" class="card-img-top img-card" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Funco de Doctor Strange</h5>
+                    <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
+                    <div class="tienda-selec">
+                        <button type="button" class="btn btn-primary">Comprar</button>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" id="card-color">
+                <img src="/img/cell.jpeg" class="card-img-top img-card" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Funco de Cell</h5>
+                    <p class="card-text">Hay en existencia: <label for="">5</label> piezas </p>
+                    <div class="tienda-selec">
+                        <button type="button" class="btn btn-primary">Comprar</button>
+                        <div class="iconos">
+                            <a href="" class="icon-a"><img src="/img/basura.png" alt=""></a>
+                            <a href="" class=""><img src="/img/editar.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
       </div>
